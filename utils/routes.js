540a4bd5efa14f7f033991069018ac5cd0b072b4
Stanley <dev.staunty@gmail.com>
2020-07-26 18:29:25 +0100

    Added Partner View to Partner Page
    Added Company List to Dashboard

--- utils/routes.js
+++ utils/routes.js
@@ -5,3 +5,4 @@ export const FETCH_ALL_PARTNERS = "partners"
 export const BLOCK_PARTNER = id => `partners/${id}/block`
 export const UNBLOCK_PARTNER = id => `partners/${id}/unblock`
 export const DELETE_PARTNER = id => `partners/${id}`
+export const PARTNER_API_CALLS = id => `api-calls/${id}`
